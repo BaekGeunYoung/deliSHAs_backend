c7285d56f12bb6829954adee4a5a33487d8489f1
BaekGeunYoung <dvmflstm@naver.com>
2020-03-07 14:35:04 +0900

    initial commit

---
+++ src/main/kotlin/com/primavera/delishas/DelishasApplication.kt
@@ -0,0 +1,11 @@
+package com.primavera.delishas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DelishasApplication
+
+fun main(args: Array<String>) {
+    runApplication<DelishasApplication>(*args)
+}
