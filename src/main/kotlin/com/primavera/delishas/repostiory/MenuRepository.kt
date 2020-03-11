a78f22657ac2a26e1add0bd9c5179aa9d3d9f332
Seo Seong Ho <swa04246@naver.com>
2020-03-11 20:05:43 +0900

    fix restaurant repository

--- src/main/kotlin/com/primavera/delishas/repostiory/MenuRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.primavera.delishas.repostiory
-
-import com.primavera.delishas.domain.Menu
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MenuRepository : JpaRepository<Menu, Long>{
-    fun getByName(name: String): Menu?
-}
