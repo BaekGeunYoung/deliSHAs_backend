c1dd16cffe18d6838d1efdf8319d59ee95e16d71
Seo Seong Ho <swa04246@naver.com>
2020-03-22 21:37:36 +0900

    rebuild restaurant service layer

--- src/main/kotlin/com/primavera/delishas/service/restaurant/RestaurantServiceImpl.kt
+++ src/main/kotlin/com/primavera/delishas/service/restaurant/RestaurantServiceImpl.kt
@@ -1,68 +1,40 @@
 package com.primavera.delishas.service.restaurant
 
 import com.primavera.delishas.domain.Restaurant
-import com.primavera.delishas.exception.RestaurantNotFoundException
+import com.primavera.delishas.dto.RestaurantDto
+import com.primavera.delishas.repostiory.MenuRepository
 import com.primavera.delishas.repostiory.RestaurantRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cache.annotation.*
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
-class RestaurantServiceImpl (
-        @Autowired val restaurantRepository: RestaurantRepository
-): RestaurantService{
-    @CachePut(value = ["menus"], key = "#restaurant.name")
-    override fun create(restaurant: Restaurant): Restaurant {
-        return restaurantRepository.save(restaurant)
+class RestaurantServiceImpl(
+        @Autowired private val restaurantRepository: RestaurantRepository,
+        @Autowired private val menuRepository: MenuRepository
+): RestaurantService {
+
+    // 테스트용 메소드
+    @CachePut(value = ["restaurants"], key = "restaurants")
+    override fun createRestaurants(restaurantsDto: MutableList<RestaurantDto>) {
+        for(restaurantDto in restaurantsDto){
+            restaurantDto.menus.map { menu -> menuRepository.save(menu) }
+            restaurantRepository.save(RestaurantDto.toRestaurant(restaurantDto))
+        }
     }
 
-    @Cacheable(value = ["menus"])
-    override fun getAll(): List<Restaurant> {
+    @Cacheable(value = ["restaurants"])
+    override fun getRestaurants(): MutableList<RestaurantDto> {
+        val restaurantsRes: MutableList<RestaurantDto> = mutableListOf()
         val restaurants = restaurantRepository.findAll()
-        if(restaurants.isEmpty()) throw RestaurantNotFoundException("can not find any menu")
 
-        // CachePut
-        for(restaurant in restaurants) create(restaurant)
-        return restaurants
+        for(restaurant in restaurants){
+            val menus = menuRepository.findByRestaurant(restaurant)
+            restaurantsRes.add(RestaurantDto.of(restaurant, menus))
+        }
+
+        return restaurantsRes
     }
 
-    override fun getById(id: Long): Restaurant {
-        val restaurant = restaurantRepository.findById(id)
-        if(!restaurant.isPresent) throw RestaurantNotFoundException("can not find menu : $id")
-
-        // CachePut
-        create(restaurant.get())
-        return restaurant.get()
-    }
-
-    @Cacheable(value = ["menus"], key = "#name")
-    override fun getByName(name: String): Restaurant {
-        val restaurant = restaurantRepository.findByName(name)
-        restaurant?: throw RestaurantNotFoundException("can not find menu : $name")
-
-        // CachePut
-        create(restaurant)
-        return restaurant
-    }
-
-//    override fun update(name: String, breakfast: String?, lunch: String?, dinner: String?): Menu {
-//        val menu = menuRepository.getByName(name)
-//        menu?: throw MenuNotFoundException("can not find menu : $name")
-//        // menu.food = food
-//        return menuRepository.save(menu)
-//    }
-
-    @CacheEvict(value = ["menus"], allEntries = true)
-    override fun deleteAll() {
-        restaurantRepository.deleteAll()
-    }
-
-    override fun deleteById(id: Long) {
-        restaurantRepository.deleteById(id)
-    }
-
-    @CacheEvict(value = ["menus"], key = "#name")
-    override fun deleteByName(name: String) {
-        restaurantRepository.deleteByName(name)
-    }
 }
